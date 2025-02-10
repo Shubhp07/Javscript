144cb326f68c5543a6d520ac68a9446aa84958c2
Shubhp07 <101512813+Shubhp07@users.noreply.github.com>
2025-02-10 09:06:50 +0000

    This is Stack and heap

---
+++ Basics/StackHeap.js
@@ -0,0 +1,15 @@
+let myName = "Shubham"
+
+let anotherName = myName;
+
+console.log(anotherName);
+console.log(myName); 
+
+let myObj = { 
+ name : "Shbham",
+ age : 23
+}
+
+
+console.log(typeof myObj);
+
