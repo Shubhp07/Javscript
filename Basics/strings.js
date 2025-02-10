0d805094a0231df1fa6b5b5a2013790f75c74609
Shubhp07 <101512813+Shubhp07@users.noreply.github.com>
2025-02-10 09:38:51 +0000

    String basics

---
+++ Basics/strings.js
@@ -0,0 +1,34 @@
+// const name = "Shubham"
+// const age = 23
+
+// console.log(`Hello my Name is ${name} and my age is ${age}`);
+
+// const game = new String("Cricate")
+
+// console.log(game.toUpperCase());
+// console.log(game.charAt(2));
+// console.log(game.indexOf('t'));
+
+// const newString = "       Shubham           "
+// console.log(newString.trim());
+
+// console.log(newString.includes("bham"));
+
+// console.log(game.split());
+
+const a = "a";
+const b = "b";
+if (a < b) {
+  
+  console.log(`${a} is less than ${b}`);
+} else if (a > b) {
+  console.log(`${a} is greater than ${b}`);
+} else {
+  console.log(`${a} and ${b} are equal.`);
+}
+
+
+
+ 
+
+
