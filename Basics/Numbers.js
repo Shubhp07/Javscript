c95788e3dfb94b80147295d42fe59f31a1bdd0a5
Shubhp07 <101512813+Shubhp07@users.noreply.github.com>
2025-02-11 14:10:40 +0000

    MathNum

---
+++ Basics/Numbers.js
@@ -0,0 +1,27 @@
+// const score = 400
+// console.log(score);
+
+// const balance  =new Number(500)
+// console.log(balance);
+
+
+// const otherNum = 342.42531;
+// console.log(otherNum.toPrecision(4));
+
+// // const hubdreds = 1000000
+// // console.log(hubdreds.toLocaleString('en_IN'));
+
+// console.log(Math);
+// console.log(Math.round(4.6));
+
+
+// console.log(Math.random()*10+1);
+
+const min = 10 
+const max =20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
