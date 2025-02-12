b9dfd330402fb4539814bf861384b5be5ff09bd6
Shubhp07 <101512813+Shubhp07@users.noreply.github.com>
2025-02-12 06:57:12 +0000

    DateTime

---
+++ Basics/DateTime.js
@@ -0,0 +1,20 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+// let myCreateDate = new Date(2025,0,23)
+// console.log(myCreateDate.toDateString());
+
+
+// let timeStamp = Date.now()
+// console.log(timeStamp);
+// console.log(timeStamp.getTime());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getHours());
+
+
+
