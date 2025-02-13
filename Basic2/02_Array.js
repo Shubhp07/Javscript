2e2a49189005814bffa549b80e8a13abcd54e503
Shubhp07 <101512813+Shubhp07@users.noreply.github.com>
2025-02-13 14:46:40 +0000

    Array1

--- Basic2/02_Array.js
+++ Basic2/02_Array.js
@@ -12,19 +12,59 @@ const newArr2 = [1,2,3,4,5,[4,3,2],5,[2,3,4,[7,8]]]
 
 const anotherArr = newArr2.flat(Infinity)
 
-console.log(anotherArr);
+// console.log(anotherArr);
 
-console.log(Array.isArray("Shubham"));
+// console.log(Array.isArray("Shubham"));
 
-console.log(Array.from("Shubham"));
+// console.log(Array.from("Shubham"));
 
-console.log(Array.from({name:"SHubham"}));
+// console.log(Array.from({name:"SHubham"}));
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1,score2,score3));
+// console.log(Array.of(score1,score2,score3));
+
+function doube (number){
+    return number*2;
+}
+
+const number = [2,3,4,5]
+const op = number.map(doube);
+console.log(op);
+
+console.log(number);
+
+
+function isLong(city){
+    return city.length > 5
+}
+
+const city = ["Pune" , "KOlhapur" , "Satara" , "Sangli"]
+const op1 = city.filter(isLong)
+console.log(op1);
+console.log(city);
+
+
+
+
+const data = "Manchester,London,Liverpool,Birmingham,Leeds,Carlisle";
+const cities = data.split(",");
+cities;
+cities.length;
+cities[0]; 
+cities[1]; 
+cities[cities.length - 1]; 
+
+data.toString();
+console.log(typeof data);
+
+
+
+
+
+
 
 
 
